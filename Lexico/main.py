4f384196855c9c1a203fd8c3b0d42b12eb1cae67
Julios <jslrocha@gmail.com>
2020-11-23 00:48:40 -0300

    ajustes

--- Lexico/main.py
+++ Lexico/main.py
@@ -1,61 +1,203 @@
+import fileinput
 import tokens
 import re
+import sys
+
+alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K',
+            'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k',
+            'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+valid_characters = [0] * 256
+
+reservedwords = tokens.Tokens().RESERVED_WORDS
+compouddelimiters = tokens.Tokens().COMPOUND
+delimitators = tokens.Tokens().DELIMITATORS
 
 
-def is_dec_valid_int(word):
-    word = word.replace(" ", "")
-    result = re.findall(r'[a-z]{1,}=[0-9]{1,}', word)
-    if len(result) > 0:
-        print("Declaração de inteiro", result[0])
-        return True
+tokenlist = []
+
+variables = {}
+
+def is_valid_character(character):
+    return True if valid_characters[ord(character)] == 1 else False
+
+def get_reserved_token(word):
+    w = reservedwords.dictionary_reservedWords.get(word)
+    if w == None:
+        return -1
+    else: return w
+
+def get_variable_type(word):
+    variables = reservedwords.variables
+    w = variables["typedef"].get(word)
+    if w != None:
+        return reservedwords.dictionary_reservedWords["typedef"]
+    w = variables["inteiro"].get(word)
+    if w != None:
+        return reservedwords.dictionary_reservedWords["inteiro"]
+
+def readline(line, list):
+    f = False;
+    w = []
+    word = ""
+    for i in range(0, len(line)):
+        if line[i] == " " and f == False:
+            continue
+        if is_valid_character(line[i]):
+            w.append(line[i])
+            f = True
+        else:
+            word = "".join(w)
+            print("palavra", word)
+            token = get_reserved_token(word)
+            print("token", token)
+            if token != -1:
+                rline = line.replace(word, "")
+                if token == 1:
+                    #main
+                    continue
+                if token == 2:
+                    #end
+                    continue
+                if token == 3:               
+                    reservedwords.typedef_declarations(rline)
+                    break
+                elif token == 9:
+                    reservedwords.inteiro_declarations(rline)
+                    break
+                elif token == 11:
+                    reservedwords.return_declarations(rline)
+                    break
+                elif token == 12:
+                    reservedwords.function_declarations(rline)
+                    break
+                else:
+                    print("erro de declaração, token n encontrado")
+                    sys.exit()
+                    return
+            else:
+                token = get_variable_type(word)
+                if token == 9:
+                    print("variavel do tipo inteiro")
+                    checking_operation(list)
+                    break
+                elif token == 3:
+                    print("variavel do tipo typedef")
+                    break
+
+def findTokens(list):
+    
+    rw = reservedwords.dictionary_reservedWords
+    cdelimiters = compouddelimiters.dictionary_delimiters_compound
+    dl = delimitators.dictionary_delimitators
+
+    for item in list:
+        w = rw.get(item)
+        if w != None:
+            tokenlist.append([reservedwords.TYPE, w, item])
+            continue
+        w = cdelimiters.get(item)
+        if w != None:
+            tokenlist.append([compouddelimiters.TYPE, w, item])
+            continue
+        w = dl.get(item)
+        if w != None:
+            tokenlist.append([delimitators.TYPE, w, item])
+            continue
+        tokenlist.append(["variable", item])
+        variables[item] = True
+
+def checking_operation(list):
+    print("list", list)
+    # a :== c +-*/ b -*/+ d ...
+    # rw = reservedwords.dictionary_reservedWords
+    # cdelimiters = compouddelimiters.dictionary_delimiters_compound
+    dl = delimitators.dictionary_delimitators
+
+    point = 0
+    for i in range(0, len(list)):
+        if i == 0:
+            if variables.get(list[i]):
+                continue
+            else:
+                print("Erro - 1")
+                sys.exit()
+        if i == 1:
+            if list[i] == "::=":
+                continue
+            else:
+                print("Erro 0")
+                sys.exit()
+        
+        if point == 0:
+            w = variables.get(list[i])
+            if w != None:
+                point = 1
+                continue
+            else:
+                try:
+                    number = int(list[i])
+                    point = 1
+                    continue
+                except:
+                    print("sequencia invalida 1")
+                    break
+        
+        elif point == 1:
+            print("list i", list[i])
+            w = dl.get(list[i])
+            print("w", w)
+            if w == 6 or w == 11 or w == 13 or w == 12:
+                point = 0
+                continue
+            elif w == 8:
+                break
+            else:
+                print("Sequencia invalida 2")
+                sys.exit()
+                break
+        if i == len(list) - 1:
+            if list[len(list) - 1] != ";":
+                print("Erro expected ;")
+    if point == 0:
+        print("Sequencia invalida 3")
+        sys.exit()
     else:
-        return False
-
-
-def is_declaration(word):
-    res = re.findall(r'^([ +]{0,}variable|array|inteiro|real)', word)
-    if (len(res) > 0):
-        print("Palavra reservada", res[0])
-        return True, res[0]
-    else:
-        print("Palavra não pertence as declarações da linguagem")
-        return False, None
-
-
-def is_statement(word):
-    res = re.findall(r'^([ ]{0,}if|else|for|while)', word)
-    if (len(res) > 0):
-        print("Palavra reservada", res[0])
-        return True, res[0]
-    else:
-        print("Palavra não pertence a linguagem")
-        return False, None
-
-
-def is_valid_statement(word):
-    word = word.replace(" ", "")
-    res = re.findall(
-        r'([a-zA-Z0-9]{1,}<[a-zA-Z0-9]{1,}|[a-zA-Z0-9]{1,}>[a-zA-Z0-9]{1,}|[a-zA-Z0-9]{1,}=[a-zA-Z0-9]{1,})', word)
-    if len(res) > 0:
-        print("Condição", res[0])
-        return True
-    else:
-        return False
-
+        print("Sequencia válida")
+        
+def end():
+    main = tokenlist[0]
+    if main[1] != reservedwords.MAIN:
+        print("Erro de sequencia")
+        sys.exit()
+    l = len(tokenlist)
+    end = tokenlist[l - 1]
+    if end[1] != reservedwords.END:
+        print("Erro de sequencia")
+        sys.exit()
 
 def main():
-    file = open("./input.txt", "r")
-    linha = 0
-    for line in file:
-        linha += 1
-        res, word = is_declaration(line)
-        if (res):
-            is_dec_valid_int(line.replace(word, ""))
+    
+    for i in alphabet:
+        valid_characters[ord(i)] = 1
+    for i in numbers:
+        valid_characters[ord(i)] = 1
 
-        else:
-            res, word = is_statement(line)
-            if (res):
-                is_valid_statement(line.replace(word, ""))
+    file = open("./input.txt", "r")
+    for line in file:
+        if line == "\n":
+            continue
+        print("line", line)
+        l = re.findall(r'[a-z]{1,}|[0-9]{1,}|::=|\+|-|\*|\/|;', line)
+        findTokens(l)
+        readline(line, l)
+    # print("tokenlist", tokenlist)
+    end()
+    for item in tokenlist:
+        print(item)
 
 
 main()
--- Lexico/tokens.py
+++ Lexico/tokens.py
@@ -1,7 +1,9 @@
 import re
-
+import sys
 
 class ReservedWords:
+    TYPE = "reserved words"
+
     MAIN = 1
     END = 2
     TYPEDEF = 3
@@ -29,11 +31,110 @@ class ReservedWords:
     STRUCTID = 25
     REPEAT = 26
 
-    def is_valid_declarations(self, word):
-        return len(re.findall(r'variable|array|inteiro|real', word)) > 0
+    dictionary_reservedWords = {
+        "main": MAIN,
+        "end": END,
+        "typedef": TYPEDEF,
+        "as": AS,
+        "variable": VARIABLE,
+        "array": ARRAY,
+        "of": OF,
+        "structure": STRUCTURE,
+        "inteiro": INTEIRO,
+        "real": REAL,
+        "return": RETURN,
+        "function": FUNCTION,
+        "procedure": PROCEDURE,
+        "if": IF,
+        "then": THEN,
+        "else": ELSE,
+        "for": FOR,
+        "to": TO,
+        "do": DO,
+        "by": BY,
+        "until": UNTIL,
+        "at": AT,
+        "with": WITH,
+        "while": WHILE,
+        "structid": STRUCTID,
+        "repeat": REPEAT
+    }
+
+    variables = {
+        "typedef": {},
+        "inteiro": {},
+        "variable": {},
+        "array": {},
+    }
+
+    def check_only_one_word(self, list):
+        print("checando se list é valida", list)
+        return False if len(list) > 1 else True
+
+    def return_declarations(self, list):
+        pass
+
+    def typedef_declarations(self, line):
+        # print("chamando line", line)
+        arr = re.findall(r'[a-z]{1,} ::= [a-zA-Z]{0,};', line)
+        print("TYPEDEF ARR", arr)
+        # print("arr", arr)
+        if len(arr) > 0:
+            arr[0] = arr[0].replace(";", "")
+            sentenca = arr[0].split("::=")     #ex: a ::= node
+            variavel_declarada = sentenca[0].replace(" ", "")
+            variavel_recebida = sentenca[1].replace(" ", "")
+            # print("declarada", variavel_declarada, "rcebida", variavel_recebida)
+            typedefdeclarations = self.variables["typedef"]
+            if len(variavel_recebida) == 0:
+                typedefdeclarations[variavel_declarada] = ""
+            else:
+                w = typedefdeclarations.get(variavel_recebida)
+                print("w", w)
+                if w == None:
+                   print("Erro ", variavel_recebida, "nao existe")
+                   sys.exit()
+                else: typedefdeclarations[variavel_declarada] = w
+        else: 
+            print("Erro declaração de palavra", line)
+            sys.exit()
+        # print("tipedefes", self.variables["typedef"])
+
+    def inteiro_declarations(self, line):
+        # print("chamando line", line)
+        arr = re.findall(r'[a-z]{1,} ::= [a-z]{1,};|[a-z]{1,} ::= [0-9]{1,};', line)
+        # print("arr", arr)
+        if len(arr) > 0:
+            arr[0] = arr[0].replace(";", "")
+            sentenca = arr[0].split("::=")     #ex: a ::= node
+            variavel_declarada = sentenca[0].replace(" ", "")
+            variavel_recebida = sentenca[1].replace(" ", "")
+            inteirodeclarations = self.variables["inteiro"]
+            w = inteirodeclarations.get(variavel_recebida)
+            if w != None:
+                # tenta transformar pra numero
+                inteirodeclarations[variavel_declarada] = w
+            else:
+                try:
+                    number = int(variavel_recebida)
+                    inteirodeclarations[variavel_declarada] = number
+                except:
+                    print("Erro ", variavel_recebida, "não é valido")
+                    sys.exit()
+        else : 
+            print("Erro declaração de palavra", line)
+            sys.exit()
+        print("inteirodeclarations", inteirodeclarations)
+
+    def function_declarations(self, line):
+        pass
+    # def is_valid_declarations(self, word):
+    #     return len(re.findall(r'variable|array|inteiro|real', word)) > 0
 
 
 class Delimitators:
+    TYPE = "Delimiter"
+
     PARLEFT = 0  # (
     PARRIGHT = 1  # )
     LESSTHEN = 2
@@ -51,7 +152,19 @@ class Delimitators:
 
     dictionary_delimitators = {
         "(": PARLEFT,
-        ")": PARRIGHT
+        ")": PARRIGHT,
+        "<": LESSTHEN,
+        ">": BIGGERTHEN,
+        "[": SBLEFT,
+        "]": SBRIGHT,
+        "+": SUM,
+        ":": COLON,
+        ";": SEMICOLON,
+        ",": COMMA,
+        "|": OR,
+        "-": SUB,
+        "/": SLASH,
+        "*": ASTRSC
     }
 
     def par_left_function(self):
@@ -63,6 +176,25 @@ class Delimitators:
     delimitatorsFunctions = [par_left_function, par_right_function]
 
 
+class CompoundDelimiters:
+    TYPE = "compound delimiter"
+    ASSIGNMENT = 1
+    ARROW = 2
+    TWOPOINTS = 3
+    BIGGEREQUAL = 4
+    LESSEQUAL = 5
+    EQUAL = 6
+
+    dictionary_delimiters_compound = {
+        "::=": ASSIGNMENT,
+        "<-": ARROW,
+        "..": TWOPOINTS,
+        ">=": BIGGEREQUAL,
+        "<=": LESSEQUAL,
+        "==": EQUAL
+    }
+
 class Tokens:
     RESERVED_WORDS = ReservedWords()
     DELIMITATORS = Delimitators()
+    COMPOUND = CompoundDelimiters()
